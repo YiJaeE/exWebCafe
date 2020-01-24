c448d0a2f92eab5d2d85873b9ba64cdc4bcff81d
YiJaeE <jinnn2181@gmail.com>
2020-01-24 18:13:56 +0900

    add javascript eventlistener

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
 const $menu = document.querySelector('.menu');
 const $items = document.querySelector('.menu-item');
+const $button = document.querySelector('.menu-button');
+const $icons = document.querySelector('.submenu');
 const $various = $menu.children;
 
 // 클릭 이벤트
 $menu.addEventListener('click', function (e) {
-  const $event = e.target.parentNode;
-  if ([...$various].map((item) => {
+  const $clickevent = e.target.parentNode;
+  ([...$various].map((item) => {
     item.classList.remove('menu-act');
   }));
-  $event.classList.add('menu-act');
+  $clickevent.classList.add('menu-act');
 });
+
+// 호버 이벤트
+// 문제: 호버 이벤트가 아무 데나 먹는다 -_-...
+// $menu.addEventListener('mouseover', function (e) {
+//   const $enterevent = e.target.parentNode;
+//   console.log($enterevent);
+//   if ($enterevent.classList.contains('menu-act')) {
+//     $enterevent.classList.add('menu-act');    
+//   }
+  
+// });
