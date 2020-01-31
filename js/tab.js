8f75d198b8de095af1152bf645e98d4de6c95acb
YiJaeE <jinnn2181@gmail.com>
2020-01-31 16:47:10 +0900

    add footer

--- js/tab.js
+++ js/tab.js
@@ -1,18 +1,18 @@
-// var panels = $('.board section');
-// var tabs = $('.tab a');
+var panels = $('.board section');
+var tabs = $('.tab a');
 
-// tabs.click(function(e){
-//   e.preventDefault();
-//   panels.removeClass('board-act');
-//   $(this).parent().parent().addClass('board-act');
-// });
+tabs.click(function(e){
+  e.preventDefault();
+  panels.removeClass('board-act');
+  $(this).parent().parent().addClass('board-act');
+});
 
 // aria version
-var tabs = $('.tab-list [role="tab"]');
+// var tabs = $('.tab-list [role="tab"]');
 
-tabs.on('click', function(e){
-  e.preventDefault();
-  $(this).attr('aria-selected', true).siblings().attr('aria-selected', false);
-  var selectedId = "#"+$(this).attr('aria-controls');
-		$(selectedId).addClass('section-act').siblings().removeClass('section-act');
-})
+// tabs.on('click', function(e){
+//   e.preventDefault();
+//   $(this).attr('aria-selected', true).siblings().attr('aria-selected', false);
+//   var selectedId = "#"+$(this).attr('aria-controls');
+// 		$(selectedId).addClass('section-act').siblings().removeClass('section-act');
+// });
