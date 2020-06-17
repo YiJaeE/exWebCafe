e3af694ec9142fb75bbf11a5fe99927aba2cf420
YiJaeE <jinnn2181@gmail.com>
2020-06-17 16:08:34 +0900

    refactor: function separation commonly used

--- js/app.js
+++ js/app.js
@@ -4,41 +4,21 @@ const $icons = $menu.querySelectorAll('.submenu a');
 const $board = document.querySelector('.board');
 
 // remove class function => main menu & tab menu
-const removeAllClass = (() => {
-  return {
-    menu() {
-      [...$menu.children].map(removeMainActive => {
-        removeMainActive.classList.remove('menu-act');
-      });
-    },
-    board() {
-      [...$board.children].map(removeTabActive => {
-        removeTabActive.classList.remove('board-act');
-      });
-    },
-  };
-})();
+const removeAllClass = position => {
+  const action = position === 'menu' ? $menu.children : $board.children;
+  [...action].map(removeAction => {
+    removeAction.classList.remove(`${position}-act`);
+  });
+};
 
 // main menu event function => click & mouseover & focus
-const mainMenuEvent = (() => {
-  return {
-    mouseEvent({ target }) {
-      const $menuChange = target.parentNode;
-      if ($menuChange.matches('.menu-item')) {
-        removeAllClass.menu();
-      } else return;
-      $menuChange.classList.add('menu-act');
-    },
-    focusEvent({ target }) {
-      const $focusMenu = target;
-      const $nowFocus = document.activeElement;
-      if (!$nowFocus.matches('.submenu a' || 'menu-item')) {
-        removeAllClass.menu();
-      }
-      $focusMenu.classList.add('menu-act');
-    },
-  };
-})();
+const mainMenuEvent = ({ type, target }) => {
+  const $target = type === 'focusin' ? target : target.parentNode;
+  if ($target.matches('.menu-item')) {
+    removeAllClass('menu');
+  } else return;
+  $target.classList.add('menu-act');
+};
 
 // sub menu insert basic icon
 [...$icons].map(basicIcon => basicIcon.setAttribute('class', 'icon-dot-circled'));
@@ -60,16 +40,16 @@ const tabActiveEvent = e => {
   e.preventDefault();
   const $panel = e.target.parentNode.parentNode;
   if (e.target.matches('.board > section > h2 > a')) {
-    removeAllClass.board();
+    removeAllClass('board');
   }
   $panel.classList.add('board-act');
 };
 
 // event Handler
 // main menu
-$menu.addEventListener('click', mainMenuEvent.mouseEvent);
-$menu.addEventListener('mouseover', mainMenuEvent.mouseEvent);
-$menu.addEventListener('focusin', mainMenuEvent.focusEvent);
+$menu.addEventListener('click', mainMenuEvent);
+$menu.addEventListener('mouseover', mainMenuEvent);
+$menu.addEventListener('focusin', mainMenuEvent);
 // sub menu
 $menu.addEventListener('mouseover', subMenuChangeIcon.activeEvent);
 $menu.addEventListener('mouseout', subMenuChangeIcon.inactiveEvent);
